05e8d0ca6257c305fb092bdbc50c0085d62a6ba5
lideryasin <32715938+lideryasin@users.noreply.github.com>
2018-07-20 17:05:19 +0300

    Update firebaseClient.js

--- src/components/firebaseClient.js
+++ src/components/firebaseClient.js
@@ -1,15 +1,14 @@
-//const firebase = require('firebase')
-//import { API_KEY } from 'react-native-dotenv'
+
 import firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCgYcHwfwhQbrx_Ux_AvCqIkSn3mzog_Mo",
-  authDomain: "react1-697c3.firebaseapp.com",
-  databaseURL: "https://react1-697c3.firebaseio.com",
-  projectId: "react1-697c3",
-  storageBucket: "react1-697c3.appspot.com",
-  messagingSenderId: "190614813192"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "3",
+  storageBucket: "",
+  messagingSenderId: ""
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
